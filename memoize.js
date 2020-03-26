31a8d1723fbae2963877b308eb3eb2d9130148e3
imerofeev <imerofeev@gmail.com>
2020-03-26 22:33:32 +0300

    add value-check

--- memoize.js
+++ memoize.js
@@ -1,17 +1,34 @@
 function memoize(func) {
   const cache = new Map();
   return function allreadyMemoized(arg) {
-    const key = arg;
-    let result = null;
+    // const key = arg;
+    // let result = null;
+    const type = typeof arg;
+    let values = cache.get(type);
 
-    if (cache.has(key)) {
-      return cache.get(key);
+    if (values === undefined) {
+      values = new Map();
+      cache.set(type, values);
     }
 
-    result = func(arg);
-    cache.set(key, result);
+    if (values.has(arg)) {
+      return values.get(arg);
+    }
+
+    const result = func(arg);
+    values.set(arg, result);
     return result;
   }
 }
+  
+//     if (cache.has(key)) {
+//       return cache.get(key);
+//     }
+
+//     result = func(arg);
+//     cache.set(key, result);
+//     return result;
+//   }
+// }
 
 module.exports = { memoize };
