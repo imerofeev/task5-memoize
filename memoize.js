12e6e43bb22de2f44c7693db9ce9ecc3399d1ee8
imerofeev <imerofeev@gmail.com>
2020-03-26 05:24:32 +0300

    add memoize function

---
+++ memoize.js
@@ -0,0 +1,18 @@
+export function memoize(func) {
+  if (typeof func === 'undefined' || typeof func !== 'function') {
+    return null;
+  }
+  const cache = {};
+  function allreadyMemoized(...args) {
+    const key = JSON.stringify(args);
+
+    if (key in cache) {
+      return cache[key];
+    }
+
+    const result = func.apply(this, args);
+    cache[key] = result;
+    return result;
+  }
+  return allreadyMemoized;
+}
