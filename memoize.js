cb7078d8bf2f4bf6526328cfc2b6963d65aa2e0a
imerofeev <imerofeev@gmail.com>
2020-03-27 13:48:11 +0300

    add many arguments memoization

--- memoize.js
+++ memoize.js
@@ -1,38 +1,57 @@
-function memoize(func) {
+function memoize(func, funcVariant = func.length) {
   if (typeof (func) !== 'function' || !arguments.length) {
     return null;
   } 
-  const cache = new Map();
-  const cacheWeak = new WeakMap();
-  return function allreadyMemoized(arg) {
-    const type = typeof arg;
+  switch (funcVariant) {
+    case 0: {
+      return function lazy (func) {
+        let created = false;
+        let value;
+        return () => {
+          if (!created) {
+            value = func();
+            created = true;
+          }
+          return value;
+        };
+      };
+    }
+    case 1:  {  
+      const cache = new Map();
+      const cacheWeak = new WeakMap();
+      return function allreadyMemoized(arg) {
+        const type = typeof arg;
 
-    if ((type === 'object' || type === 'function') && arg !== null) {
-      if (cacheWeak.has(arg)) {
-        return cacheWeak.get(arg);
+        if ((type === 'object' || type === 'function') && arg !== null) {
+          if (cacheWeak.has(arg)) {
+            return cacheWeak.get(arg);
+          }
+
+          const result = func(arg);
+          cacheWeak.set(arg, result);
+          return result;
+        }
+
+        let values = cache.get(type);
+
+        if (values === undefined) {
+          values = new Map();
+          cache.set(type, values);
+        }
+
+        if (values.has(arg)) {
+          return values.get(arg);
+        }
+
+        const result = func(arg);
+        values.set(arg, result);
+        return result;
       }
-
-      const result = func(arg);
-      cacheWeak.set(arg, result);
-      return result;
     }
-
-    let values = cache.get(type);
-
-    if (values === undefined) {
-      values = new Map();
-      cache.set(type, values);
+    default: {
+      const allreadyMemoized = memoize(arg1 => memoize((...args) => func(arg1, ...args), funcVariant - 1));
+      return (arg1, ...args) => allreadyMemoized(arg1)(...args);
     }
-
-    if (values.has(arg)) {
-      return values.get(arg);
-    }
-
-    const result = func(arg);
-    values.set(arg, result);
-    return result;
-
-    
   }
 }
 
