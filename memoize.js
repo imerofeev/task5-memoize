d907f7849e400e65a32751b3ae829b22872897bf
imerofeev <imerofeev@gmail.com>
2020-03-30 07:26:35 +0300

    refactoring memoize function

--- memoize.js
+++ memoize.js
@@ -1,11 +1,11 @@
-function memoize(func, funcVariant = func.length) {
-  if (typeof (func) !== 'function' || !arguments.length) {
-    return null;
-  } 
+function memoize(func) {
+  if (typeof (func) !== 'function' || !arguments.length) return null;
   const cache = new Map();
   const cacheWeak = new WeakMap();
-  switch (funcVariant) {
-    case 0: {
+  return function allreadyMemoized(...args) {
+    let key = '';
+    const type = typeof arg;
+    if (!args.length){
       return function lazy (func) {
         let created = false;
         let value;
@@ -18,55 +18,26 @@ function memoize(func, funcVariant = func.length) {
         };
       };
     }
-    case 1:  {  
-      return function allreadyMemoized(arg) {
-        const type = typeof arg;
-
-        if ((type === 'object' || type === 'function') && arg !== null) {
-          if (cacheWeak.has(arg)) {
-            return cacheWeak.get(arg);
-          }
-
-          const result = func(arg);
-          cacheWeak.set(arg, result);
-          return result;
-        }
-
-        let values = cache.get(type);
-
-        if (values === undefined) {
-          values = new Map();
-          cache.set(type, values);
-        }
-
-        if (values.has(arg)) {
-          return values.get(arg);
-        }
-
-        const result = func(arg);
-        values.set(arg, result);
-        return result;
+    else if ((type === 'object' || type === 'function') && arg !== null) {
+      if (cacheWeak.has(arg)) {
+        return cacheWeak.get(arg);
+      }
+      const result = func(arg);
+      cacheWeak.set(arg, result);
+      return result;
+    }
+    else {
+      for (let i = 0; i < args.length; i += 1) {
+        key += args[i] + typeof args[i];
       }
     }
-    default: {
-      return function allreadyMemoized(...arg) {
-        const hashName = JSON.stringify(arg);
-        let result = null;
-  
-        if (cashe.has(hashName)) {
-          return cashe.get(hashName);
-        }
-  
-        result = func(...arg);
-        cashe.set(hashName, result);
-        return result;
-      }
+    if (cache.has(key)) {
+      return cache.get(key);
     }
-    // default: {
-    //   const allreadyMemoized = memoize(arg1 => memoize((...args) => func(arg1, ...args), funcVariant - 1));
-    //   return (arg1, ...args) => allreadyMemoized(arg1)(...args);
-    // }
-  }
+    const result = func.apply(this, args);
+    cache.set(key, result);
+    return result;
+  };
 }
 
 module.exports = { memoize };
